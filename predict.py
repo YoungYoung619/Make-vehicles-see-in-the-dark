bc7aab464c061f38b04dfe98db91691992097642
YoungYoung619 <940752885@qq.com>
2019-04-26 13:06:48 +0800

    predict

---
+++ predict.py
@@ -0,0 +1,90 @@
+"""
+Copyright (c) College of Mechatronics and Control Engineering, Shenzhen University.
+All rights reserved.
+
+Description :
+
+
+Author：Team Li
+"""
+import tensorflow as tf
+import numpy as np
+import logging
+import os
+import glob
+
+from net.u_net import u_net
+from dataset.bdd_daytime import bdd_daytime
+
+from skimage import io
+import cv2
+
+tf.app.flags.DEFINE_string(
+    'checkpoint_dir', './checkpoint',
+    'The path to a checkpoint from which to fine-tune.')
+
+
+FLAGS = tf.app.flags.FLAGS
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+input = tf.placeholder(shape=[None, 139, 209, 3], dtype=tf.float32)
+groundtruth = tf.placeholder(shape=[None, 139, 209, 3], dtype=tf.float32)
+global_step = tf.Variable(0, trainable=False, name='global_step')
+
+
+def build_graph(input):
+    output = u_net(input, is_training=False)
+
+    return output
+
+def main(_):
+    output = build_graph(input)
+
+    logger.info('Total trainable parameters:%s'%(str(np.sum([np.prod(v.get_shape().as_list()) \
+                                                             for v in tf.trainable_variables()]))))
+
+    saver = tf.train.Saver()
+
+    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+    init = tf.global_variables_initializer()
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+        pd = bdd_daytime(batch_size=1, for_what='train', shuffle=True)
+
+        if ckpt:
+            logger.info('loading %s...'%str(ckpt.model_checkpoint_path))
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            logger.info('load %s success...'%str(ckpt.model_checkpoint_path))
+        else:
+            raise ValueError('checkpoint_dir may be wrong..')
+
+        while True:
+            dark_img, input_img = pd.load_batch()
+            out_img = sess.run(output, feed_dict={input:dark_img})
+            dark_img = dark_img[0]
+            out_img = out_img[0]
+
+            dark_img = np.uint8((dark_img + 1.) * 255 / 2)
+            out_img = np.uint8((out_img + 1.) * 255 / 2)
+
+            dark_img = cv2.cvtColor(dark_img, cv2.COLOR_RGB2BGR)
+            out_img = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
+            dark_img = cv2.resize(dark_img, (418, 278))
+            out_img = cv2.resize(out_img, (418,278))
+            cv2.imshow('dark_img', dark_img)
+            cv2.imshow('test', out_img)
+            cv2.waitKey()
+            cv2.destroyAllWindows()
+
+
+
+
+
+
+if __name__ == '__main__':
+    tf.app.run()
+
