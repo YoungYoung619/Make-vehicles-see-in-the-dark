90cb26274b0a091e39a0040338558e5a5cd39f1a
YoungYoung619 <940752885@qq.com>
2019-11-02 13:19:41 +0800

    'add_gaussian'

--- dataset/bdd_daytime.py
+++ dataset/bdd_daytime.py
@@ -81,7 +81,7 @@ class bdd_daytime(object):
             raw_imgs = []
             dark_imgs= []
             for i in range(self.batch_size):
-                raw_img, dark_img = self.load_ong_img(aug=True)
+                raw_img, dark_img = self.load_one_img(aug=True)
                 raw_imgs.append(raw_img)
                 dark_imgs.append(dark_img)
             return np.array(raw_imgs), np.array(dark_imgs)
@@ -89,7 +89,7 @@ class bdd_daytime(object):
             raw_imgs = []
             dark_imgs = []
             for i in range(self.batch_size):
-                raw_img, dark_img = self.load_ong_img()
+                raw_img, dark_img = self.load_one_img()
                 raw_imgs.append(raw_img)
                 dark_imgs.append(dark_img)
             return np.array(raw_imgs), np.array(dark_imgs)
@@ -97,7 +97,7 @@ class bdd_daytime(object):
             raw_imgs = []
             dark_imgs = []
             for i in range(self.batch_size):
-                raw_img, dark_img = self.load_ong_img()
+                raw_img, dark_img = self.load_one_img()
                 raw_imgs.append(raw_img)
                 dark_imgs.append(dark_img)
             return np.array(raw_imgs), np.array(dark_imgs)
@@ -105,7 +105,7 @@ class bdd_daytime(object):
             raise ValueError('wrong!!')
 
 
-    def load_ong_img(self, normalize=True, aug=True):
+    def load_one_img(self, normalize=True, aug=True):
         if self.shuffle:
             file_name = random.sample(self.imgs_full_name, 1)[0]
         else:
@@ -119,19 +119,34 @@ class bdd_daytime(object):
 
             hist = np.random.randint(0, 10, 1)
             if hist >= 4:
-                alpha = np.random.randint(10, 100, 1) / 100
+                alpha = np.random.randint(10, 50, 1) / 100
                 img = transform.match_histograms(img, self.std_dark*alpha)
 
             if aug:
                 seq_det = data_aug_seq.to_deterministic()
                 img = seq_det.augment_images([img.astype(np.uint8)])[0]
 
-            img = cv2.resize(img, (img_size[1], img_size[0]))
-            raw_img = cv2.resize(raw_img, (img_size[1], img_size[0])).astype(np.float32)
+            dark_img = transform.resize(img, (img_size[0], img_size[1]))
+            light_img = transform.resize(raw_img, (img_size[0], img_size[1]))
 
+            n_gaussian = random.randint(5, 10)
+            points = []
+            radius = []
+            for i in range(n_gaussian):
+                x = random.randint(0, img_size[1]-1)
+                y = random.randint(0, img_size[0]-1)
+                radiu = random.uniform(50, max(*img_size)/3)
+                radius.append(radiu)
+                points.append(np.array([x, y]))
 
-            return (raw_img*2./255.-1., img*2./255. - 1.) if normalize \
-                else (raw_img.astype(np.uint8), img.astype(np.uint8))  ##normalize to -1 -- 1
+            hm = self.__heat_map(img_size, points, radius)
+            hm = np.greater(hm, 0.8).astype(np.float32)
+            hm_ = 1. - hm
+            dark_img = dark_img*np.expand_dims(hm, axis=-1) + light_img*np.expand_dims(hm_, axis=-1)
+            cv2.imshow('hm', hm)
+
+            return (light_img, dark_img) if normalize \
+                else ((light_img*255.).astype(np.uint8), (dark_img*255.).astype(np.uint8))  ##normalize to -1 -- 1
 
         elif self.for_what == 'test':
             raw_img = self.read_one_img(file_name)
@@ -178,27 +193,84 @@ class bdd_daytime(object):
         Return:
             ndarray of img after reducing the light
         """
-        g = np.random.randint(0, 10, 1)
-        a = np.random.randint(0, 10, 1)
-        if g >= 4:
-            # print('gamma yes')
-            gamma = np.random.randint(5, 25, 1) / 10
-        else:
-            gamma = 1
-        if a >= 4:
-            alpha = np.random.randint(10, 100, 1) / 100
-        else:
-            alpha = 1
+        gamma = np.random.uniform(2, 6, 1)
+        alpha = np.random.uniform(0.1, 0.5, 1)
 
         return (exposure.adjust_gamma(img, gamma)*alpha).astype(np.uint8)
+
+    def __heat_map(self, img_size, points, sigmas):
+        """produce a heat map(gray scale) according the points
+        Args:
+            img_size: img height and width
+            points: ndarray or list, represents the coordinate of points. (x, y)
+            sigma: control the heap point range
+        return:
+            a heap map with the shape (h, w)
+        Example:
+            aa = heat_map(img_size=(224, 224), points=[[50, 50], [100, 100]], sigma=2)
+            cv2.imshow('test', aa)
+            cv2.waitKey()
+            cv2.destroyAllWindows()
+        """
+        assert self.__legal_points(img_size, points)
+
+        x = np.arange(0, img_size[1], 1)
+        y = np.arange(0, img_size[0], 1)
+        z = np.swapaxes(np.array(np.meshgrid(x, y)), axis1=0, axis2=2)
+        z = np.swapaxes(z, axis1=0, axis2=1)
+        heat_map = np.array([self.__gaussian_2d(z, point, sigma=sigma) for point, sigma in zip(points, sigmas)])
+        heat_map = np.max(heat_map, axis=0)
+        return heat_map / np.max(heat_map)
+
+
+    def __legal_points(self, img_size, points):
+        """judge all points whether in img range
+        Args:
+            img_size: [img_h, img_w]
+            points: a list of point in x,y coordinate.
+        Return:
+            if all legal, True
+            else, False
+        """
+        for point in points:
+            if point[0] < 0 or point[0] > img_size[1] - 1:
+                return False
+            if point[1] < 0 or point[1] > img_size[0] - 1:
+                return False
+        return True
+
+
+    def __gaussian_2d(self, point, mu, sigma):
+        """2d gaussion function
+        Args:
+            point: 2d point coordinate
+            mu: 2d mean value
+            sigma: the standard deviation in gaussian func
+        Return:
+            a img with the shape (h, w)
+        Example:
+            x = np.arange(0, 224, 1)
+            y = np.arange(0, 224, 1)
+            z = np.swapaxes(np.array(np.meshgrid(x, y)), axis1=0, axis2=2)
+            z = gauss_2d(z, mu=(100,100), sigma=2)
+            cv2.imshow('test', z)
+            cv2.waitKey()
+            cv2.destroyAllWindows()
+        """
+        h = point.shape[0]
+        w = point.shape[1]
+        point = np.reshape(point, newshape=[-1, 2])
+        score = np.exp(-(np.sum(np.square(np.array(point)-np.array(mu)), axis=-1))/(2*sigma**2))
+
+        return np.reshape(score, newshape=[h, w])
 
 ## a seq of img augumentation ##
 data_aug_seq = iaa.SomeOf(2,[
 
         # Small gaussian blur with random sigma between 0 and 0.5.
         # But we only blur about 50% of all images.
-        iaa.Sometimes(0.5,
-                      iaa.GaussianBlur(sigma=(0, 0.5))
+        iaa.Sometimes(0.8,
+                      iaa.GaussianBlur(sigma=(1, 3))
                       ),
 
         # Add gaussian noise.
@@ -209,18 +281,20 @@ data_aug_seq = iaa.SomeOf(2,[
         iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.01 * 255), per_channel=0.1)]
         , random_order=True)  # apply augmenters in random order
 
-#
-# if __name__ == '__main__':
-#     provider = bdd_daytime(10, 'train', shuffle=True)
-#
-#     for i in range(100):
-#         img, dark_img = provider.load_ong_img(normalize=True)
-#         img = (img+1.)*255/2
-#         dark_img = (dark_img+1.)*255/2
-#         plt.subplot(121)
-#         io.imshow(img.astype(np.uint8))
-#         plt.subplot(122)
-#         io.imshow(dark_img.astype(np.uint8))
-#         plt.show()
-#
-#     pass
+
+if __name__ == '__main__':
+    provider = bdd_daytime(10, 'train', shuffle=True)
+
+    for i in range(100):
+        img, dark_img = provider.load_one_img(normalize=False)
+        cv2.imshow('light', img)
+        cv2.imshow('dark', dark_img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        # plt.subplot(121)
+        # io.imshow(img.astype(np.uint8))
+        # plt.subplot(122)
+        # io.imshow(dark_img.astype(np.uint8))
+        # plt.show()
+
+    pass
